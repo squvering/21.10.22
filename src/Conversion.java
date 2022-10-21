8ffd2218d6be0f075096a2ff9c8080dc6d79edf6
squvering <koppev8888@yandex.ru>
2022-10-21 14:30:39 +0300

    Roman Numerals Encoder

---
+++ src/Conversion.java
@@ -0,0 +1,61 @@
+public class Conversion {
+
+    public String solution(int n) {
+        String s = "";
+        int cnt = 0;
+
+        while(n >= 1000) {
+            s += "M";
+            n-=1000;
+        }
+        while(n >= 900) {
+            s += "CM";
+            n-=900;
+        }
+        while(n >= 500) {
+            s += "D";
+            n-=500;
+        }
+        while(n >= 400) {
+            s += "CD";
+            n-=400;
+        }
+        while(n >= 100) {
+            s += "C";
+            n-=100;
+        }
+        while(n >= 90) {
+            s += "XC";
+            n-=90;
+        }
+        while(n >= 50) {
+            s += "L";
+            n-=50;
+        }
+        while(n >= 40) {
+            s += "XL";
+            n-=40;
+        }
+        while(n >= 10) {
+            s += "X";
+            n-=10;
+        }
+        while(n >= 9) {
+            s += "IX";
+            n-=9;
+        }
+        while(n >= 5) {
+            s += "V";
+            n-=5;
+        }
+        while(n >= 4) {
+            s += "IV";
+            n-=4;
+        }
+        while(n >= 1) {
+            s += "I";
+            n-=1;
+        }
+        return s;
+    }
+}
